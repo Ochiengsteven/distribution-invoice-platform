92b4a28844bc02665fcae363fd43b55b4a32b27b
JerryLegend254 <jerryokuto713@gmail.com>
2024-08-31 13:15:43 +0300

    feat: homepage responsiveness

--- src/app/(main)/page.js
+++ src/app/(main)/page.js
@@ -16,14 +16,14 @@ export default function Home() {
   return (
     <main className="flex flex-col gap-6">
       <Header />
-      <div className="flex w-[1100px] self-center text-center">
+      <div className="flex flex-col md:flex-row w-[600px] lg:w-[1100px] self-center text-center">
         <p className="text-indigo-950 text-8xl">*</p>
-        <h1 className="text-xl xl:text-7xl font-black uppercase mx-auto text-indigo-950">
+        <h1 className="text-2xl md:text-5xl xl:text-7xl font-black uppercase mx-auto text-indigo-950">
           Effortless invoice management from warehouse to customer
         </h1>
-        <p className="text-indigo-950 text-8xl self-end">*</p>
+        <p className="text-indigo-950 text-8xl md:self-end">*</p>
       </div>
-      <div className="flex gap-8 self-center">
+      <div className="flex flex-col md:flex-row gap-8 self-center">
         {attrArr.map((att) => (
           <div
             key={att}
@@ -34,12 +34,12 @@ export default function Home() {
           </div>
         ))}
       </div>
-      <div className="flex flex-col items-center justify-center mt-6">
+      <div className="flex flex-col items-center justify-center mt-6 gap-4">
         <Image src="/home-dip.png" alt="hero-image" width={560} height={300} />
-        <div className="bg-indigo-950 px-6 py-4 flex gap-4 rounded-full absolute right-[-20px] rotate-[-20deg] font-bold">
+        <div className="bg-indigo-950 px-6 py-4 flex gap-4 rounded-full md:absolute md:right-[-20px] md:rotate-[-20deg] font-bold">
           <p className="text-white">Have questions?</p>
         </div>
-        <div className="bg-indigo-950 px-12 py-4 flex rounded-full absolute left-[-40px] font-bold bottom-12">
+        <div className="bg-indigo-950 px-12 py-4 flex rounded-full md:absolute md:left-[-40px] font-bold md:bottom-12">
           <p className="text-white">Your #1 Invoice Management Buddy!</p>
         </div>
       </div>
